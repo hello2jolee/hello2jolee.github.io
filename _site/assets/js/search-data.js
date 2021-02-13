177fd346e33f625a90b809160b4b1cd320bd8c2d
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-02-13 14:28:46 +0900

    초기 셋팅 - 기본 정보 입력

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Resume",
+        "excerpt":"반갑습니다 : ) 저는 이지은입니다. ","categories": [],
+        "tags": [],
+        "url": "https://hello2jolee.github.io/resume/"
+      }]
